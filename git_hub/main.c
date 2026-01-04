69746a5f4d30eaff0e1aec5a7415376e9006b2b0
Md Hasemi Rafsan Jani Shohan <113755788+Rafsan12345@users.noreply.github.com>
2026-01-04 08:55:56 +0600

    Add files via upload

---
+++ git_hub/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello, EEE!\n");
+    printf("Hello, cse!\n");
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("Hello, World!!! %zu\n", i);
+        printf("Hello, EEE!!! %zu\n", i);
+        printf("Hello, cse!!! %zu\n", i);
+
+        /* code */
+    }
+    
+    return 0;
+}
